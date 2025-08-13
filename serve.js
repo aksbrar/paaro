bcb0e809b68a6267bd4559d868100591b90f0ca9
aksbrar <aksbrar34@gmail.com>
2025-08-12 21:53:53 -0400

    serving static

--- serve.js
+++ serve.js
@@ -1,13 +1,17 @@
 // imports
 import http from "node:http"
+import { serveStatic } from "./utils/serveStatic.js"
 
 // port
 const port = 8000
 
+// cwd
+const __dirname = import.meta.dirname
+
 // create server
-const server = http.createServer((req, res) => {
-  if (req.url = "/"){
-    res.end("Hi")
+const server = http.createServer(async (req, res) => {
+  if (req.url != "/api"){
+    const content = await serveStatic(req, __dirname)
   }
 })
 
