154a3db66796a7dc0bab9cfa1dfd881f7d56dae9
aksbrar <aksbrar34@gmail.com>
2025-08-13 12:33:39 -0400

    ai is working perfectly, now time to do some tweaks, showing results, changing appearnace, try and catch errors

--- ai-handiling/aiResponse.js
+++ ai-handiling/aiResponse.js
@@ -1,6 +1,34 @@
 // imports
 import { sendRes } from "../utils/sendRes.js"
+import OpenAI from "openai"
+import 'dotenv/config'
+// other way
+// import {dotenv} from "dotenv"
+// dotenv.config()
 
-export const getAiRes = (res, body) => {
-  sendRes(res, 200, "application/json", JSON.stringify(body))
+// api key
+const key = process.env.OPENAI_KEY
+
+// initiate ai
+const myAI = new OpenAI({
+  "apiKey" : key
+})
+
+
+export const getAiRes = async (res, body) => {
+  const response = await myAI.chat.completions.create({
+    model : "gpt-5",
+    messages : [
+      {
+        role : "assistant",
+        content : "You are just a random, nice guy sending kind replies"
+      },
+      {
+        role : 'user',
+        content : body.question
+      }
+    ]
+  })
+
+  sendRes(res, 200, "application/json", JSON.stringify(response.choices[0].message.content))
 }
